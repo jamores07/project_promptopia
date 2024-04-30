ca2e5d8c90fa2bc2018c776b9f93c58a621a4c3b
John Amores <kayak.stated_0b@icloud.com>
2024-04-30 23:04:23 +0000

    Navbar WIP

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,16 @@ import Feed from "@components/Feed"
 
 const Home = () => {
   return (
-    <section className="w-full flex center flex-col">
+    <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> AI Powered Prompts </span>
       </h1>
-      <p className="desc test-center">
-        Promptopia is an open-source AI prompting
-        tool for the modern world to discover, create
-        and share creative prompts
-      </p>
+      <p className="desc text-center">
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
 
       {/* FEED */}
       <Feed />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"; //  <-- Needed to use client functionality in NextJS; components are "server" by default
+
+import Link from "next/link"
+import Image from "next/image"
+import { useState, useEffect } from "react"
+import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image 
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+      </Link>
+      <p className='logo_text'>Promptopia</p>
+    </nav>
   )
 }
 
