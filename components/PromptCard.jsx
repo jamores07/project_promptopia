284c6f338ec928ac7dc867320cd2831f254f2de8
John Amores <kayak.stated_0b@icloud.com>
2024-05-13 23:05:27 +0000

    Feed and Prompt cards WIP --02:24:07

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
 
-const PromptCard = () => {
+import { useState } from 'react';
+import Image from 'next/image';
+import { useSession } from "next-auth/react";
+import { usePathnam, useRouter } from 'next/navigation';
+
+
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={() => {}}
+        >
+          <Image
+            src={post.create.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
