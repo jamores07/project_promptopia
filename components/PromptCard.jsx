d06244ddfe505fed01734ab91dcc5d8f73c53a7b
John Amores <kayak.stated_0b@icloud.com>
2024-05-15 16:12:58 +0000

    Added Profile page, functionality WIP --02:48:26

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,12 +3,13 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { useSession } from "next-auth/react";
-import { usePathnam, useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const { data: session } = useSession();
+  const pathName = usePathname();
   const [copied, setCopied] = useState("");
 
 
@@ -61,6 +62,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
               {post.tag}
             </p>
 
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
