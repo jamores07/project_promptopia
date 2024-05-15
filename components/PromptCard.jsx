8fb4ba5cb4611c53cde3c48f89b9496ac828309d
John Amores <kayak.stated_0b@icloud.com>
2024-05-15 15:40:59 +0000

    Prompt Card completed --02:32:42

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,7 +9,15 @@ import { usePathnam, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const { data: session } = useSession();
-  console.log(session)
+  const [copied, setCopied] = useState("");
+
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000);
+  }
+
 
   return (
     <div className="prompt_card">
@@ -35,9 +43,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           </div>
         </div>
         
-
+         <div className="copy_btn" onClick={handleCopy}>  
+          <Image 
+            src={ copied === post.prompt
+              ? '/assets/icons/tick.svg'
+              : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          />
+         </div>
       </div>
-
+            <p className='my-4 font-satoshi text-sm text-gray-700'>
+              {post.prompt}
+            </p>
+            <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>
+              {post.tag}
+            </p>
 
     </div>
   )
