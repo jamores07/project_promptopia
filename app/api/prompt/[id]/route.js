b502a9b2f28dd4769b386099df2e77f0ba5c522a
John Amores <kayak.stated_0b@icloud.com>
2024-05-16 01:40:52 +0000

    Completed CRUD functionality for Application

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,61 @@
+import { connectDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+//GET 
+export const GET = async (request, { params }) => {
+    try {
+        await connectDB()
+
+        const prompt = await Prompt.findById(params.id).populate("creator")
+        
+        if (!prompt) return new Response("Prompt Not Found", { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+
+    } catch (error) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
+};
+
+//PATCH
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag}= await request.json();
+
+    try {
+        await connectDB();
+
+        // Find existing prompt by ID
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if (!existingPrompt) {
+            return new Response("Prompt not found", { status: 404 });
+        }
+
+
+        // Updates prompt with new data
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+        return new Response(JSON.stringify(existingPrompt), { status: 200 });
+
+    } catch (error) {
+        return new Response("Failed to update prompt", { status: 500 });
+    }
+};
+    
+//DELETE
+export const DELETE = async (request, { params }) => {
+
+    try { 
+        await connectDB();
+
+        // Find existing prompt by ID and remove it
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 });
+
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 });
+    }
+};
